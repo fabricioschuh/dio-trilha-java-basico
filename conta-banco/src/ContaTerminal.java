78d61e2f12fa67805dd10c2dd4ca630cc5b83672
Fabricio Schuh <fabricio.schuh@sap.com>
2023-08-29 20:40:39 -0300

    adicionando programa da conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        double saldo = 0;
+        double valorSolicitado = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        saldo = scanner.nextDouble();
+        valorSolicitado = scanner.nextDouble();
+
+        if (saldo >= valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
+        }
+        else {
+            System.out.println("Saldo insuficiente");
+        }
+
     }
 }
